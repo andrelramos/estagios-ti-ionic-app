e85eed8dc29a1a43ba6e5423e7c006334e8fc467
André Luiz de Oliveira Ramos <andrel.ramos97@gmail.com>
2016-04-08 18:57:23 -0300

    Loading jobs information

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -6,4 +6,11 @@ angular.module('starter.controllers', [])
     }
 })
 
-.controller('JobsCtrl', function($scope) {});
+.controller('JobsCtrl', function($scope, $http) {
+    //http://storage.scrapinghub.com/items/49316?apikey=a88f05aed82b4473ae3a641048e58257&format=json
+    $http.get("https://storage.scrapinghub.com/items/49316/1/87?apikey=a88f05aed82b4473ae3a641048e58257&format=json")
+        .success(function(data){
+            $scope.jobs = data;
+        });
+
+});
