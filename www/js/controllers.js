d71eacbe1a4682650e48a0c7e46e1966696d8cf0
André Luiz de Oliveira Ramos <andrel.ramos97@gmail.com>
2016-04-08 17:51:42 -0300

    Removing goTo scope function from MainCtrl

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,10 +4,6 @@ angular.module('starter.controllers', [])
     $scope.openUrl = function(url) {
         window.open(url, '_blank', 'location=yes');
     }
-
-    $scope.goTo = function(location) {
-        $location.path(location);
-    }
 })
 
 .controller('JobsCtrl', function($scope) {});
